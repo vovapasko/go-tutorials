d09bff7d7c7c27a2ff1454a2272d5c6dbb335769
vovapasko <vovapasko1699@gmail.com>
2021-02-14 15:09:11 +0100

    Provided select goroutines

--- golangr_turorial/channels.go
+++ golangr_turorial/channels.go
@@ -9,8 +9,41 @@ func main() {
 	// funcChannels()
 	// syncBuffer()
 	// channelSync()
-	receiveOnlyTutorial()
-	sendOnlyTutorial()
+	// receiveOnlyTutorial()
+	// sendOnlyTutorial()
+	selectChannel()
+}
+
+func selectChannel() {
+	c3 := make(chan string)
+	c4 := make(chan string)
+
+	go f3(c3)
+	go f4(c4)
+
+	for {
+		select {
+		case msg1 := <-c3:
+			fmt.Println(msg1)
+		case msg2 := <-c4:
+			fmt.Println(msg2)
+		}
+	}
+	fmt.Println("Goroutines finished")
+}
+
+func f3(c chan string) {
+	for {
+		time.Sleep(1 * time.Second)
+		c <- "3"
+	}
+}
+
+func f4(c chan string) {
+	for {
+		time.Sleep(1 * time.Second)
+		c <- "4"
+	}
 }
 
 func receiveOnlyTutorial() {
