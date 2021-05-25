fc60209030e1589017b1f02605291974f880b872
vovapasko <vovapasko1699@gmail.com>
2021-05-25 21:18:49 +0200

    Provided bot's template

---
+++ simple_chatbot/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var botName string = "Teresh"
+var yearCreated int = 2021
+
+func main() {
+	fmt.Printf("Hi! I am %s \n", botName)
+	fmt.Printf("I was created in %d \n", yearCreated)
+}
