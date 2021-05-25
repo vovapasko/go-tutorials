3a557903eea952f0f55ff46f76b344ad54eb8ef6
vovapasko <vovapasko1699@gmail.com>
2021-05-25 21:45:55 +0200

    Provided taking name from input

--- simple_chatbot/main.go
+++ simple_chatbot/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 var botName string = "Teresh"
 var yearCreated int = 2021
@@ -8,4 +12,14 @@ var yearCreated int = 2021
 func main() {
 	fmt.Printf("Hi! I am %s \n", botName)
 	fmt.Printf("I was created in %d \n", yearCreated)
+	fmt.Println("Please, remind me your name.")
+	name := getStrInput()
+	fmt.Printf("Nice to meet you, %s \n", name)
+}
+
+func getStrInput() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("> ")
+	value, _ := reader.ReadString('\n')
+	return value
 }
